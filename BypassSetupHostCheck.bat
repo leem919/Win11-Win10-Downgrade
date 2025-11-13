60deedd06ea0349d870c57c7144c88d0d49dfadb
leem919 <leem919@gmail.com>
2025-11-12 18:55:08 -0800

    Update BypassSetupHostCheck.bat

--- BypassSetupHostCheck.bat
+++ BypassSetupHostCheck.bat
@@ -1,3 +1,4 @@
+@echo off
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo Requesting administrator privileges...
@@ -5,11 +6,8 @@ if %errorLevel% neq 0 (
     exit /b
 )
 
-cd C:\$WINDOWS.~BT\Sources
-copy SetupHost.exe SetupHost2.exe
-reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\$WINDOWS.~BT\Sources\SetupHost2.exe" /t REG_SZ /d "WIN8RTM" /f
-
 for /f "tokens=*" %%a in ('powershell -Command "(Get-WmiObject win32_process -Filter \"name like '%%SetupHost.exe'\").CommandLine"') do set originalCmd=%%a
-set modifiedCmd=%originalCmd:SetupHost.exe=SetupHost2.exe%
+
 taskkill /F /IM SetupHost.exe
-start "" %modifiedCmd%
+
+start cmd /c "set __COMPAT_LAYER=WIN8RTM && %originalCmd%"
